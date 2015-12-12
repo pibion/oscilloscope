432c54c96cab546a77ac2b851cf7056836f13ad4
Amy Roberts <amy.l.roberts@usd.edu>
2015-12-11 22:35:31 -0600

    working code for multiple paths

--- scope.js
+++ scope.js
@@ -1,39 +1,59 @@
+// http://stackoverflow.com/questions/23305785/draw-line-with-d3-js-using-separate-fixed-x-y-input-arrays
+// http://bost.ocks.org/mike/nest/
+// http://bost.ocks.org/mike/join/
+// http://bost.ocks.org/mike/selection/
+
+
 (function() {
-  JSONData = [
-  { "id": 3, "created_at": "Sun May 05 2013", "amount": 12000},
-  { "id": 1, "created_at": "Mon May 13 2013", "amount": 2000},
-  { "id": 2, "created_at": "Thu Jun 06 2013", "amount": 17000},
-  { "id": 4, "created_at": "Thu May 09 2013", "amount": 15000},
-  { "id": 5, "created_at": "Mon Jul 01 2013", "amount": 16000}
-]
-
-  var data = JSONData.slice()
-  var format = d3.time.format("%a %b %d %Y")
-  var amountFn = function(d) { return d.amount }
-  var dateFn = function(d) { return format.parse(d.created_at) }
-
-  var x = d3.time.scale()
-    .range([10, 280])
-    .domain(d3.extent(data, dateFn))
-
-  var y = d3.scale.linear()
-    .range([180, 10])
-    .domain(d3.extent(data, amountFn))
-  
+  // make yourself an SVG container
+  // in the DOM
   var svg = d3.select("#plot").append("svg:svg")
   .attr("width", 300)
   .attr("height", 200)
+  
+  // and now let's plot some lines
+  // in that SVG container
+  // using this here data
+  var volt_data = [[2,2.3,2.6,2,1.9],
+                   [2.4,4,2,4.5,3],
+                   [1.3,2.1,2.6,1.2,0.8]]
+  var time_data = [0.1,0.2,0.3,0.4,0.5]
+  //var trace_data = d3.zip(volt_data, time_data)
+  //console.log(trace_data)
+  
+  // not currently using these functions
+  // keeping them around 'cause they might be useful
+  var voltFn = function(d) {return d}
+  var timeFn = function(i) {return time_data[i]}
+  
+  // define "scale" objects that 
+  // scale our data into a reasonable number of pixels
+  // these scales are used in the definition 
+  // of the d3.svg.line()
+  var x_scale = d3.scale.linear()
+    .domain(d3.extent(time_data))
+    .range([0, 300])
 
+  var y_scale = d3.scale.linear()
+    .domain([5, 0])
+    .range([200, 2])
+  
+  var trace = d3.svg.line()
+      .x(function(d,i) { return x_scale(time_data[i]); })
+      .y(function(d,i) { return y_scale(d); })
+  
 var refreshGraph = function() {
-    var circles = svg.selectAll("circle").data(data,dateFn)
+    var traces = svg.selectAll("path")
+      .data(volt_data)
+      
+    //console.log(traces)
     
-    circles.enter()
-     .append("svg:circle")
-     .attr("r", 4)
-     .attr("cx", function(d) { return x(dateFn(d)) })
-     .attr("cy", function(d) { return y(amountFn(d)) })
+    traces.enter()
+     .append("svg:path")
+     .attr("class","line")
+     .attr("d",function(d,i) {return trace(d,i)})
      
-     circles.exit()
+     traces.exit()
      .remove() 
 }
 
